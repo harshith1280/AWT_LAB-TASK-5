c99b5e5e62db0f4148e361053d4fc4212e28e407
pandemharshithreddy <pandemharshith@gmail.com>
2025-03-15 14:26:07 +0530

    Module.js
    task 5
    5 b

---
+++ Module.js
@@ -0,0 +1,6 @@
+const OS = requrie('OS');
+console.log('OS Platform :${os.Platform()}');
+console.log('OS Architecture : ${OS.arch())');
+console.log('Total memory:${OS.totalmem()}bytes')
+console.log('Free memory :${OS.Freemem()');
+console.log('Home Directory:${OS.homedir()}');
